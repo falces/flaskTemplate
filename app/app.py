36e2b42d60038c104e1925b92e8375dc626674b2
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-06-11 17:07:27 +0200

    ADD: Swagger

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from config.routes.routes import routes
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app, template={
+    "info": {
+        "title": "My Flask API",
+        "description": "An example API using Flask and Swagger",
+        "version": "1.0.0"
+    }
+})
 
 app.register_blueprint(routes)
 
--- app/config/routes/routes.py
+++ app/config/routes/routes.py
@@ -7,4 +7,13 @@ routes.register_blueprint(routesv1, url_prefix='/v1')
 
 @routes.route('/', methods=['GET'])
 def index():
+    """
+    This is an example endpoint that returns 'Hello, World!'
+    ---
+    responses:
+        200:
+            description: A successful response
+            examples:
+                application/json: "Hello, World!"
+    """
     return redirect("http://localhost:8001/v1/users/2?name=Javi", code=302)
