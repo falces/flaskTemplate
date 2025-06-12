abbe2bd0dbca39da3f33fd3ad55422796523aed9
Javier Rodríguez <javier.rodriguez@create-store.com>
2025-06-12 12:35:57 +0200

    FIX: delete unused library

--- app/config/routes/routesv1.py
+++ app/config/routes/routesv1.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 from modules.Countries.routes import countriesRoutes
+from modules.Users.routes import usersRoutes
 
 routesv1 = Blueprint('v1', __name__)
 
+routesv1.register_blueprint(usersRoutes, url_prefix='/users')
 routesv1.register_blueprint(countriesRoutes, url_prefix='/countries')
